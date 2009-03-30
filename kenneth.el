8c14a62806282bdbe6b2315aded315117f18ab84
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-03-30 23:23:48 +0200

    gist.el and some more files to ruby-mode

--- kenneth.el
+++ kenneth.el
@@ -3,9 +3,12 @@
 
 ;; Capfiles are Ruby too
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 
 ;; Vendor stuff
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/egg"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/gist.el"))
 
 ;; egg
 (require 'egg)
